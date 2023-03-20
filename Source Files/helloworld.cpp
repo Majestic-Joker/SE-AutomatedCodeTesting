849ac52f2fa0aefaea110fe38273bff74a8e2ec1
Joshua Beaty <68184850+Majestic-Joker@users.noreply.github.com>
2023-03-20 15:41:21 -0500

    added a cpp test file

---
+++ Source Files/helloworld.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+int main(){
+    int number = 42;
+    string username = "Captain Obvious";
+
+    cout << "Hello World!\n" << "Username: " << username << " Number: " << number << endl; 
+
+    return 0;
+}
