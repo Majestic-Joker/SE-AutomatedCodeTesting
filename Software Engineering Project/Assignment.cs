a706fe889f293bc070b1bd52502e3336b5a59deb
Paxton Proctor <61135201+paxtonproctor@users.noreply.github.com>
2023-04-19 20:08:40 -0500

    Add files via upload

---
+++ Software Engineering Project/Assignment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software_Engineering_Project
+{
+    // Class for Assignments
+    public class Assignment
+    {
+        public string AssignmentName { get; set; }
+        public string AssignmentDirectory { get; set; }
+        public List<Submission> Submissions { get; set; }
+        public string InputFilepath { get; set; }
+        public string OutputFilepath { get; set; }
+    }
+}
