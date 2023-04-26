ca89fa4419787de8f1f6aedf4bc9c2cc5591a6b3
Joshua Beaty <68184850+Majestic-Joker@users.noreply.github.com>
2023-04-26 18:35:40 -0500

    Overrode ToString method in Result.cs

--- Software Engineering Project/Result.cs
+++ Software Engineering Project/Result.cs
@@ -14,5 +14,29 @@ namespace Software_Engineering_Project
         public string ExeOutput { get; set; }
         public BuildResult BuildResult { get; set; }
         public CppCompilation CppCompilation { get; set; }
+
+        public override string ToString()
+        {
+            string resultAsString = string.Empty;
+
+            resultAsString += $"Compiled:\t\t{Compiled}\n";
+            resultAsString += $"Did .exe run:\t\t{RunComplete}\n";
+            resultAsString += $"Output matches:\t\t{OutputMatchesExpected}\n";
+            resultAsString += $"Output Match Percentage:\t\t{MatchPercentage}%\n";
+            resultAsString += $"Output from .exe:\n{ExeOutput}\n";
+
+            if(CppCompilation.HasErrors){
+                resultAsString += $"\nCompilation Errors:\n";
+                foreach(var message in CppCompilation.Diagnostics.Messages)
+                    resultAsString += $"\t{message.ToString()}\n";
+            }
+
+            if(BuildResult.OverallResult != BuildResultCode.Success){
+                resultAsString += "\nBuild Error:\n";
+                resultAsString += $"\t{BuildResult.Exception.ToString()}\n";
+            }
+
+            return resultAsString;
+        }
     }
 }
